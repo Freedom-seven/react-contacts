8ea9b3f9331243e640fd04f34c4906e57eccdd4a
FREEDOM <freewhynane62@gmail.com>
2021-07-07 06:12:03 +0000

    add state with array

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contacts: [
+        {
+          name: "Freedom",
+          phoneNumber: "0240020202",
+          location: "Kasoa"
+        },
+        {
+          name: "Fiona",
+          phoneNumber: "0240030303",
+          location: "East Legon"
+        },
+        {
+          name: "Patrick",
+          phoneNumber: "0240040404",
+          location: "Trasacco"
+        },
+        {
+          name: "Sika",
+          phoneNumber: "0240050505",
+          location: "Obolo Estate"
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
