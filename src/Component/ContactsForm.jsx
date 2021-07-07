ebe538577a16c44f703c7b1a49e84e99d6c2581e
FREEDOM <freewhynane62@gmail.com>
2021-07-07 09:33:24 +0000

    remove footer

--- src/Component/ContactsForm.jsx
+++ src/Component/ContactsForm.jsx
@@ -22,6 +22,7 @@ class ContactsForm extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.addContact(this.state);
+    alert("Contact added")
     this.setState({
         name: "",
         phoneNumber: "",
@@ -31,42 +32,39 @@ class ContactsForm extends Component {
 
   render() {
     return (
-        <>
-            <form onSubmit={this.handleSubmit} className="contactForm">
-                <h1>Create Contact</h1>
-                <div>
-                <label htmlFor="name">Name: </label>
-                <input className="inputField"
-                    type="text"
-                    name="name"
-                    value={this.state.name}
-                    onChange={this.handleChange}
-                />
-                </div>
-                <div>
-                <label htmlFor="phoneNumber">Phone Number: </label>
-                <input className="inputField"
-                    type="number"
-                    name="phoneNumber"
-                    value={this.state.phoneNumber}
-                    onChange={this.handleChange}
-                />
-                </div>
-                <div>
-                <label htmlFor="locaton">Location: </label>
-                <input className="inputField"
-                    type="text"
-                    name="location"
-                    value={this.state.location}
-                    onChange={this.handleChange}
-                />
-                </div>
-                <div>
-                <button className="submit">Add Contact</button>
-                </div>
-            </form>
-            <div className="footer"></div>
-      </>
+        <form onSubmit={this.handleSubmit} className="contactForm">
+            <h1>Create Contact</h1>
+            <div>
+            <label htmlFor="name">Name: </label>
+            <input className="inputField"
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+            />
+            </div>
+            <div>
+            <label htmlFor="phoneNumber">Phone Number: </label>
+            <input className="inputField"
+                type="number"
+                name="phoneNumber"
+                value={this.state.phoneNumber}
+                onChange={this.handleChange}
+            />
+            </div>
+            <div>
+            <label htmlFor="locaton">Location: </label>
+            <input className="inputField"
+                type="text"
+                name="location"
+                value={this.state.location}
+                onChange={this.handleChange}
+            />
+            </div>
+            <div>
+            <button className="submit">Add Contact</button>
+            </div>
+        </form>
     );
   }
 }
