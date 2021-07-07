10fc5e3a9150d0b62016f847fea2b907a60375af
FREEDOM <freewhynane62@gmail.com>
2021-07-07 06:37:32 +0000

    add component

---
+++ src/Component/Contacts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = ({contact, index}) => {
+    return (
+        <div className="contact" key={index}>
+          <h1>Name: {contact.name}</h1>
+          <p>Phone Number: {contact.phoneNumber}</p>
+          <p>Location: {contact.location}</p>
+          <hr />
+        </div>
+    )
+}
+
+export default Contacts;
