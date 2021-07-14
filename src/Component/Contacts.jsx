29643aadc962c63b9c19044bc76afc23c6612faa
FREEDOM <freewhynane62@gmail.com>
2021-07-14 08:22:32 +0000

    add delete function

--- src/Component/Contacts.jsx
+++ src/Component/Contacts.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-const Contacts = ({contact, index}) => {
+
+const Contacts = ({contact, deleteContact}) => {
+  const handleDelete = (e) => {
+    deleteContact(contact.id);
+  }
     return (
-        <div style={{margin: "2rem"}} className="contact" key={index}>
-          <h2>Name: {contact.name}</h2>
+        <div style={{margin: "2rem"}} className="contact">
+          <h3>Name: {contact.name}</h3>
           <p>Phone Number: {contact.phoneNumber}</p>
           <p>Location: {contact.location}</p>
+          <div>
+            <button className="btn btn-success edit">Edit</button>
+            <button className="btn btn-danger delete" onClick={handleDelete}>Delete</button>
+            </div>
           <hr />
         </div>
     )
