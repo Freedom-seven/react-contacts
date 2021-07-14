3dcf2b5245fc47dab652fa687bbd6ae5abc19788
FREEDOM <freewhynane62@gmail.com>
2021-07-14 08:30:48 +0000

    add edit contact props

--- src/Component/ContactsList.jsx
+++ src/Component/ContactsList.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Contacts from './Contacts';
 
-const ContactsList = ({contacts, deleteContact}) => {
+const ContactsList = ({contacts, deleteContact, editContact}) => {
     const contactsList = contacts.map((contact) => {
         return (
-          <Contacts contact={contact} deleteContact={deleteContact} />
+          <Contacts contact={contact} deleteContact={deleteContact} editContact={editContact} />
         );
       });
 
