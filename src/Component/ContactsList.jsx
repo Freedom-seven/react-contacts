ad3affa0ebe2b0cf13ea43674de82ca21b477b3c
FREEDOM <freewhynane62@gmail.com>
2021-07-14 07:11:45 +0000

    add function

--- src/Component/ContactsList.jsx
+++ src/Component/ContactsList.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Contacts from './Contacts';
 
-const ContactsList = () => {
+const ContactsList = ({contacts}) => {
+    const contactsList = contacts.map((contact) => {
+        return (
+          <Contacts contact={contact} />
+        );
+      });
+
     return (
         <div>
-            
+            {contactsList}
         </div>
     );
 }
