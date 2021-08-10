48e8c997003aed5ca38bfdf8793dc239b9d16619
FREEDOM <freewhynane62@gmail.com>
2021-08-10 15:22:22 +0000

    add component reducer

---
+++ src/Reducer/ContactReducer.js
@@ -0,0 +1,40 @@
+const initialState = {
+    contacts: [
+        {
+          name: "Freedom",
+          phoneNumber: "0240020202",
+          location: "Kasoa",
+          id: Math.random().toString()
+        },
+        {
+          name: "Fiona",
+          phoneNumber: "0240030303",
+          location: "East Legon",
+          id: Math.random().toString()
+        },
+        {
+          name: "Patrick",
+          phoneNumber: "0240040404",
+          location: "Trasacco",
+          id: Math.random().toString()
+        },
+        {
+          name: "Sika",
+          phoneNumber: "0240050505",
+          location: "Obolo Estate",
+          id: Math.random().toString()
+        },
+      ]
+}
+
+const contactReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD_CONTACT":
+            return state
+    
+        default:
+            return state;
+    }
+}
+
+export default contactReducer
