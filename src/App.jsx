505fb88bf99b8352022268250c28afb3ceff9929
FREEDOM <freewhynane62@gmail.com>
2021-08-10 15:20:47 +0000

    add id to contact array

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,25 @@ class App extends Component {
           name: "Freedom",
           phoneNumber: "0240020202",
           location: "Kasoa",
+          id: Math.random().toString()
         },
         {
           name: "Fiona",
           phoneNumber: "0240030303",
           location: "East Legon",
+          id: Math.random().toString()
         },
         {
           name: "Patrick",
           phoneNumber: "0240040404",
           location: "Trasacco",
+          id: Math.random().toString()
         },
         {
           name: "Sika",
           phoneNumber: "0240050505",
           location: "Obolo Estate",
+          id: Math.random().toString()
         },
       ],
     };
