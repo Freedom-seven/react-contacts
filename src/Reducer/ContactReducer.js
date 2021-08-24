6cc2ad88c2837e6c997760668dd1106120f31bd6
FREEDOM <freewhynane62@gmail.com>
2021-08-24 16:45:47 +0000

    add actions to reducer

--- src/Reducer/ContactReducer.js
+++ src/Reducer/ContactReducer.js
@@ -31,7 +31,19 @@ const contactReducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD_CONTACT":
             return { ...state.contacts, contacts: [...state.contacts, action.payload] };
-    
+
+        case "EDIT_CONTACT":
+         const newContacts = state.contacts.map((contact) =>
+        contact.id === action.payload.id ? action.payload : contact
+      )
+
+        return {...state, contacts: newContacts};
+
+      case "DELETE_CONTACT":
+        const contactSaved = state.contacts.filter((contact) => {
+          return contact.id !== action.payload;
+        }); 
+        return {...state, contacts: contactSaved};
         default:
             return state;
     }
