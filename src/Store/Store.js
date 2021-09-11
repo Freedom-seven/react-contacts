1b31697736459d0ea0945c4db34c40c51fb01076
FREEDOM <freewhynane62@gmail.com>
2021-09-11 16:50:02 +0000

    import firebase dependencies

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,6 +1,17 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import contactReducer from "../Reducer/ContactReducer";
+import firebase from "../firebase/config";
+import { getFirebase, reactReduxFirebase } from "react-redux-firebase";
+import { getFirestore, reduxFirestore } from "redux-firestore";
+import thunk from "redux-thunk";
 
-const Store = createStore(contactReducer)
 
-export default Store
+
+const Store = createStore(contactReducer, compose(applyMiddleware(thunk.withExtraArgument({
+    getFirebase, getFirestore
+})),
+    reactReduxFirebase(firebase),
+    reduxFirestore(firebase)
+))
+
+export default Store;
