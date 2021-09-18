94590f0255f3b0c2a6febb4d873b583fd36b0142
FREEDOM <freewhynane62@gmail.com>
2021-09-18 08:05:26 +0000

    add firebase database

--- src/actions/contactAction.js
+++ src/actions/contactAction.js
@@ -1,21 +1,33 @@
+import { getFirestore } from "redux-firestore";
+
 export const addContact = (contact) => {
-    contact.id = Math.random().toString();
-    return {
-        type: "ADD_CONTACT",
-        payload: contact
-    }
-}
+  contact.id = Math.random().toString();
+
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("contacts")
+      .add(contact)
+      .then((docs) => {
+        console.log(docs);
+      });
+  };
+
+  //return {
+  //   type: "ADD_CONTACT",
+  //  payload: contact
+  //}
+};
 
 export const editContact = (updatedContact) => {
-    return {
-        type: "EDIT_CONTACT",
-        payload: updatedContact
-    }
-}
+  return {
+    type: "EDIT_CONTACT",
+    payload: updatedContact,
+  };
+};
 
 export const deleteContact = (contact_id) => {
-    return {
-        type: "DELETE_CONTACT",
-        payload: contact_id
-    }
-}
+  return {
+    type: "DELETE_CONTACT",
+    payload: contact_id,
+  };
+};
