b2f5638747ece0c0039e4d71a82b5f34b42b00da
FREEDOM <freewhynane62@gmail.com>
2021-10-08 01:15:54 +0000

    add auth actions

---
+++ src/actions/authActions.js
@@ -0,0 +1,60 @@
+export const registerWithEmail = (email, password) => {
+    return (dispatch, state, { getFirebase }) => {
+      const firebase = getFirebase();
+  
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then((user) => {
+          console.log(user);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+  };
+  
+  export const loginWithGoogle = () => {
+    return (dispatch, state, { getFirebase }) => {
+      const firebase = getFirebase();
+      var provider = new firebase.auth.GoogleAuthProvider();
+  
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          var user = result.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+  };
+  
+  export const loginWIthEmail = (email, password, replace) => {
+    return (dispatch, state, { getFirebase }) => {
+      const firebase = getFirebase();
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((res) => {
+          replace("/");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }; 
+  };
+  
+  export const logout = (replace) => {
+    return (dispatch, state, { getFirebase }) => {
+      const firebase = getFirebase();
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+            
+        });
+    };
+  };
