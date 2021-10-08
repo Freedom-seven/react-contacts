084f10c740f642e4d6db448ec3759285f5ccb20d
FREEDOM <freewhynane62@gmail.com>
2021-10-08 01:13:57 +0000

    render router

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,14 @@
 import React, { Component } from "react";
-import ContactsForm from "./Component/ContactsForm";
-import "./App.css";
-import ContactsList from "./Component/ContactsList";
+import Router  from "./Router";
+
 
 class App extends Component {
 
-  handleAddContact = (newContact) => {
-    newContact.id = Math.random().toString();
-    this.setState({
-      contacts: [...this.state.contacts, newContact],
-    });
-  };
-
-  handleDeleteContact = (contactId) => {
-    const contactSaved = this.state.contacts.filter((contact) => {
-      return contact.id !== contactId;
-    });
-    this.setState({ contacts: contactSaved });
-  };
-
-  handleEditContact = (updatedContact) => {
-    this.setState({
-      contacts: this.state.contacts.map((contact) =>
-        contact.id === updatedContact.id ? updatedContact : contact
-      ),
-    });
-  };
-
   render() {
     return (
-      <div className="completeContact">
-        <ContactsForm addContact={this.handleAddContact} />
-        <ContactsList />
-      </div>
+      <>
+      <Router />
+      </>
     );
   }
 }
